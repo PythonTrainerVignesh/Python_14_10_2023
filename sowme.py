3bd55ed7348f3c5f31a63e542536d7cbbe6f0c2e
Sowmebalu <128220624+Sowmebalu@users.noreply.github.com>
2023-10-19 17:04:56 +0530

    Update sowme.py

--- sowme.py
+++ sowme.py
@@ -87,6 +87,99 @@ Escapte Character - Used to break that character as an ending syntax character
            ""learning""
            """Python""" 
 
+# Integer
+  print(55555)
 
+# Float
+  print(555.55)
+  print(55.)
+  print(.5)
 
+# Boolean
+  print(True, False)
+  print(5 > 9)
+  print(5/9)
+
+# Variable
+  x = 5
+  y = 9
+  result = x * y
+  print('Multiplication of', x, 'and', y, 'is', result)
+
+# Operators (7) & Comparison Operators (6)
+# BODMAS
+  print(5 + 9)
+  print(5 - 9)
+  print(5 / 9)
+  print(5 * 9)
+  print(5 ** 9 ** 2)  # Exponential
+  print(5 % 9 % 2)  # Modulus
+  print(26 // 9)  # Floor division
+
+# Comparison Operators (6)
+  print(5 > 9)
+  print(5 < 9)
+  print(5 >= 9)
+  print(5 <= 9)
+  print(5 == 9)  # Compare
+  print(5 != 9)  # not equal to
+
+# input (Always string)
+  a = input('Enter first no: ')
+  b = input('Enter second no: ')
+  print(int(a) + int(b))
+  print(float(a) + float(b))
+  print(str(a) + str(b))
+  print(bool(a > b) + bool(a < b))
+
+name = 'Bob'
+print("Hello", name + '!')
+print(f"Hello {name}!")
+
+a = 5
+b = 8
+
+print(f"Sum of {a} and {b} is {a + b}.")
+
+# Conditions
+a = int(input('Enter any no: '))
+
+if a < 0:
+    print(f"{a} is a negative no.")
+elif a >= 0 and a <= 100:
+    print(f"{a} is between 0 and 100.")
+elif 101 <= a <= 200:
+    print(f"{a} is between 101 and 200.")
+else:
+    print(f"{a} is greater than 200.")
+
+if a < 50 or a > 100:
+    print('')
+
+# If the no is even and less than 100,
+# If the no is even and divisible by 5,
+# if the no is odd and greater than 100,
+
+word = input('Enter a word: ')
+
+if word == "Spathiphyllum":
+    pass
+elif word == "spathiphyllum":
+    pass
+else:
+    pass
+
+a = 50000 / 0.7
+result = round(a, 2)
+print(a, result)
+
+# Index no and Slicing
+
+a = 'asdasfsdgdfgdfhdfhg'
+
+print(a[1])
+# a[start:stop:step]
+print(a[3] == 'i')
+print(len(a))
+print(a[:len(a)//2])
 
