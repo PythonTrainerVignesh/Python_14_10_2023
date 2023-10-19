ff8249869405578d566613bdf01bf4c30b4affa3
Sowmebalu <128220624+Sowmebalu@users.noreply.github.com>
2023-10-19 16:16:02 +0530

    Update sowme.py

--- sowme.py
+++ sowme.py
@@ -1,1 +1,15 @@
+Python is an 'Interpreter'
+LEFT to RIGHT
+Interprets compiles each line of source code one by one. in interpret source code is open (not safe)  Adv. -  Easy to debug. 
+Compiler compiles the entire code by combining it as a package. If there is an error, it will just say it is an error rather than explaining about the type or location of the error.
+In compiler source code is safe
 
+Difference between Interpreter and Compiler --> Interpreter codes are risky. In compiler, codes are safe as it cannot be cracked easily.
+
+Low level language - Machine readable
+High level language - User readable 
+
+Python is a high level language.
+Python is CASE-SENSITIVE
+
+  
